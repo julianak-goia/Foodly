8fd62fe1445adb65e78a1f7587c6a04b9f860856
Juliana <juliana.goia@allycode.com.br>
2025-09-22 20:45:20 -0300

    feat: add promo banner to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Header from "./components/header";
 import Search from "./components/search";
 import CategoryList from "./components/category-list";
+import Image from "next/image";
 
 export default function Home() {
   return (
@@ -11,6 +12,16 @@ export default function Home() {
       </div>
       <div className="px-5 pt-6">
         <CategoryList />
+      </div>
+
+      <div className="px-5 pt-6">
+        <Image
+          src="/promo-banner-01.svg"
+          alt="Até 30% de desconto em pizzas"
+          height={0}
+          width={0}
+          className="h-auto w-full object-contain"
+        />
       </div>
     </>
   );
