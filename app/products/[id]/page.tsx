b7c2eeff96b8e4a67dc8ce016e0ce6cf7fdfc6d5
Juliana <juliana.goia@allycode.com.br>
2025-09-25 21:41:42 -0300

    feat: add product detail component

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,12 +1,7 @@
-import {
-  calculateProductTotalPrice,
-  formatCurrency,
-} from "@/app/_helpers/price";
 import { db } from "@/app/lib/prisma";
-import Image from "next/image";
 import { notFound } from "next/navigation";
 import ProductImage from "./_components/product-image";
-import DiscountBadge from "@/app/components/discount-badge";
+import ProductDetails from "./_components/product-detail";
 
 interface ProductPageProps {
   params: {
@@ -24,6 +19,17 @@ const ProductsPage = async ({ params: { id } }: ProductPageProps) => {
     },
   });
 
+  const juices = await db.product.findMany({
+    where: {
+      category: {
+        name: "Sucos",
+      },
+    },
+    include: {
+      restaurant: true,
+    },
+  });
+
   //se não tiver um produto com o id vai para a pagina de 404
   if (!product) {
     return notFound();
@@ -33,43 +39,7 @@ const ProductsPage = async ({ params: { id } }: ProductPageProps) => {
     <div>
       <ProductImage product={product} />
 
-      <div className="p-5">
-        <div className="flex items-center gap-[0.375rem]">
-          <div className="relative h-6 w-6">
-            <Image
-              src={product.restaurant.imageURL}
-              alt={product.restaurant.name}
-              fill
-              className="rounded-full object-cover"
-            />
-          </div>
-
-          <span className="text-xs text-muted-foreground">
-            {product.restaurant.name}
-          </span>
-        </div>
-
-        <h1 className="mb-2 mt-1 text-xl font-semibold">{product.name}</h1>
-
-        <div className="flex justify-between">
-          <div className="">
-            <div className="flex items-center gap-2">
-              <h2 className="text-xl font-semibold">
-                {formatCurrency(calculateProductTotalPrice(product))}
-              </h2>
-              {product.discountPercentage > 0 && (
-                <DiscountBadge product={product} />
-              )}
-            </div>
-
-            {product.discountPercentage > 0 && (
-              <p className="text-sm text-muted-foreground">
-                De: {formatCurrency(Number(product.price))}
-              </p>
-            )}
-          </div>
-        </div>
-      </div>
+      <ProductDetails product={product} complementaryProducts={juices} />
     </div>
   );
 };
