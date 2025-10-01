8513884e16e4ce9899358f5c3c78ef12d578fde3
Juliana <juliana.goia@allycode.com.br>
2025-09-30 21:26:15 -0300

    feat: add restaurant page

--- app/components/restaurant-item.tsx
+++ app/components/restaurant-item.tsx
@@ -3,6 +3,7 @@ import { BikeIcon, HeartIcon, StarIcon, TimerIcon } from "lucide-react";
 import Image from "next/image";
 import { formatCurrency } from "../_helpers/price";
 import { Button } from "./ui/button";
+import Link from "next/link";
 
 interface RestaurantItemProps {
   restaurant: Restaurant;
@@ -10,45 +11,50 @@ interface RestaurantItemProps {
 
 const RestaurantItem = ({ restaurant }: RestaurantItemProps) => {
   return (
-    <div className="min-w-[266px] max-w-[266px] space-y-3">
-      <div className="relative h-[136px] w-full">
-        <Image
-          src={restaurant.imageURL}
-          alt={restaurant.name}
-          fill
-          className="rounded-lg object-cover shadow-md"
-        />
+    <Link
+      className="min-w-[266px] max-w-[266px]"
+      href={`/restaurants/${restaurant.id}`}
+    >
+      <div className="w-full space-y-3">
+        <div className="relative h-[136px] w-full">
+          <Image
+            src={restaurant.imageURL}
+            alt={restaurant.name}
+            fill
+            className="rounded-lg object-cover shadow-md"
+          />
 
-        <div className="absolute left-2 top-2 flex items-center gap-[2px] rounded-full bg-white px-2 py-[2px]">
-          <StarIcon size={12} className="fill-yellow-400 text-yellow-400" />
-          <span className="text-xs font-semibold">5.0</span>
-        </div>
-
-        <Button className="absolute right-2 top-2 h-7 w-7 rounded-full bg-gray-700">
-          <HeartIcon className="h-fit w-fit fill-white" size={12} />
-        </Button>
-      </div>
-      <div>
-        <h3 className="text-sm font-semibold">{restaurant.name}</h3>
-        <div className="flex gap-3">
-          <div className="flex items-center gap-1">
-            <BikeIcon className="text-primary" size={14} />
-            <span className="text-xs text-muted-foreground">
-              {Number(restaurant.deliveryFee) === 0
-                ? "Entrega grátis"
-                : formatCurrency(Number(restaurant.deliveryFee))}
-            </span>
+          <div className="absolute left-2 top-2 flex items-center gap-[2px] rounded-full bg-white px-2 py-[2px]">
+            <StarIcon size={12} className="fill-yellow-400 text-yellow-400" />
+            <span className="text-xs font-semibold">5.0</span>
           </div>
 
-          <div className="flex items-center gap-1">
-            <TimerIcon className="text-primary" size={14} />
-            <span className="text-xs text-muted-foreground">
-              {restaurant.deliveryTimeMinutes} min
-            </span>
+          <Button className="absolute right-2 top-2 h-7 w-7 rounded-full bg-gray-700">
+            <HeartIcon className="h-fit w-fit fill-white" size={12} />
+          </Button>
+        </div>
+        <div>
+          <h3 className="text-sm font-semibold">{restaurant.name}</h3>
+          <div className="flex gap-3">
+            <div className="flex items-center gap-1">
+              <BikeIcon className="text-primary" size={14} />
+              <span className="text-xs text-muted-foreground">
+                {Number(restaurant.deliveryFee) === 0
+                  ? "Entrega grátis"
+                  : formatCurrency(Number(restaurant.deliveryFee))}
+              </span>
+            </div>
+
+            <div className="flex items-center gap-1">
+              <TimerIcon className="text-primary" size={14} />
+              <span className="text-xs text-muted-foreground">
+                {restaurant.deliveryTimeMinutes} min
+              </span>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
